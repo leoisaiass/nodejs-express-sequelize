77355b0f3ad12e98c7d8dd3ee24829427655b920
Leonardo Isaias <leoisaias135@gmail.com>
2026-08-11 08:02:08 -0300

    Aula 2, 3 e 4 - Desenvolvendo a API com CRUD e tabelas com relações

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 const express = require("express");
+const routes = require("./routes/index.js");
 
 const app = express();
 
-app.use(express.json());
-
-app.get("/teste", (req, res) => {
-  res.status(200).send({ mensagem: "boas-vindas à API" });
-});
+routes(app);
 
 module.exports = app;
